7a90f29143975ecaefb9358feb5d8b558896f419
susradza <susi.radzanowski@vodafone.com>
2025-10-14 13:44:50 +0200

    Update 13.10 Ordnerstruktur Neu

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log
+
+let theLink = document.getElementById("Link1");
+let theDiv = document.getElementById("div1");
+
+theLink.innerHTML="Neuer Text";
+theLink.style.color = "red";
+theLink.style.backgroundColor = "#cec";
+
+function klickReaction() {}
